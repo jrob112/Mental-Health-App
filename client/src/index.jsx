52fbcb888ed2643e08f8f506a4b9e079f95b53a3
jrob112 <josh.roberts0527@gmail.com>
2024-05-10 19:12:59 -0500

    added the react-router to index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <App />
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
 const container = document.getElementById('root');
 
-const root = ReactDOM.createRoot(container);
-root.render(<App />);
+const root = createRoot(container);
+root.render(<RouterProvider router={router} />);
