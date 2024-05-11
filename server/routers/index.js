df34fa50e38bca7e18182704f7d5be093db52924
AlexPHebert2000 <aphebert9994@gmail.com>
2024-05-11 15:01:09 -0500

    merge routers

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const weatherController = require("./weatherController");
+const weatherController = require('./weatherController');
+const journalController = require('./journalController');
+const userController = require('./userController');
 
 const router = express.Router();
 
+// User Routers
+router.route('/:userId').get(userController.getUser);
+router.route('/user').post(userController.addUser);
+
+// Weather Routers
 router.route('/weather').get(weatherController.getWeather);
+
 
 module.exports = router;
