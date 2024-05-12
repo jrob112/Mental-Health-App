8f541a5606cf6cf5a8e35d0332b031b9eca2865b
Alex Hebert <aphebert9994@gmail.com>
2024-05-12 02:38:51 +0000

    ADD clear db and seed with data

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,1 +1,46 @@
+(async() => {
+await require('..')
+
 const { User, Journals, Moods, Habits } = require('.');
+const loremIpsum = require('lorem-ipsum').LoremIpsum;
+
+//setup lorem-ipsum
+const lorem = new loremIpsum();
+
+//clear database
+User.destroyAll();
+Journals.destroyAll();
+Moods.destroyAll();
+Habits.destroyAll();
+
+user1 = await User.create({
+    username: lorem.generateWords(1),
+    location: lorem.generateWords(2),
+})
+await Journals.create({
+    title: lorem.generateWords(5),
+    body: lorem.generateParagraphs(3),
+    UserId: user1.id,
+})
+await Journals.create({
+    title: lorem.generateWords(5),
+    body: lorem.generateParagraphs(3),
+    UserId: user1.id,
+})
+
+user2 = await User.create({
+    username: lorem.generateWords(1),
+    location: lorem.generateWords(2),
+})
+await Journals.create({
+    title: lorem.generateWords(5),
+    body: lorem.generateParagraphs(3),
+    UserId: user2.id,
+})
+await Journals.create({
+    title: lorem.generateWords(5),
+    body: lorem.generateParagraphs(3),
+    UserId: user2.id,
+})
+
+})()
