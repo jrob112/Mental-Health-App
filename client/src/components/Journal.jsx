5cde47e103151d3c330288962dcb133ca0e73e40
AlexPHebert2000 <aphebert9994@gmail.com>
2024-05-12 19:44:29 -0500

    ADD skeleton component

--- client/src/components/Journal.jsx
+++ client/src/components/Journal.jsx
@@ -1,8 +1,30 @@
+import React, { useState } from "react";
 
 const Journal = () => {
+
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
   return (
     <>
       <h1>JOURNAL</h1>
+      <input
+        type="text"
+        value={title}
+        name="title"
+        onChange={(e) => {setTitle(e.target.value)}}
+        placeholder="Title"
+      ></input>
+      <textarea
+        type="text"
+        value={body}
+        name="body"
+        onChange={(e) => {setBody(e.target.value)}}
+        placeholder="What's on your mind?"
+      ></textarea>
+      <button
+        type="submit"
+      >Save Journal</button>
     </>
   )
 }
