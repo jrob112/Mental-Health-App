20aa4c3dce0f52c2497188793ac29f58c4096c6f
AlexPHebert2000 <aphebert9994@gmail.com>
2024-05-13 19:17:09 -0500

    ADD

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import HomePage from './HomePage.jsx';
 import Journal from './Journal.jsx';
 import Login from './Login.jsx';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/home' element={<HomePage />} />
-      <Route path='/journal' element={<Journal />} />
-      <Route path='/' element={<Login />}/>
-    </Routes>
+    <>
+      <Routes>
+        <Route path='/home' element={<HomePage />} />
+        <Route path='/journal' element={<Journal />} />
+        <Route path='/' element={<Login />}/>
+      </Routes>
+    </>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -72,4 +72,8 @@ app.get('/auth/google/callback',
         failureRedirect: '/'
 }));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(DIST_PATH, 'index.html'));
+});
+
 app.listen(PORT, () => {console.info(`Server listening on 127.0.0.1:${PORT}`)});
