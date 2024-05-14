4766834603b80be39be8d601b4860a2a9daa0064
uhrinuh <nasri.arina@gmail.com>
2024-05-14 15:39:09 -0700

    client dist files i need for material ui or else it breaks idk

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Weather from './Weather.jsx';
 import { Button } from '@mui/material';
-import { purple } from '@mui/material/colors';
 
 const HomePage = () => {
   return (
