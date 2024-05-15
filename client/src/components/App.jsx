5091552d903d3a281d80257ce1a6eeacc5ecb32a
Hackman78 <pathenry231@gmail.com>
2024-05-15 16:21:38 -0500

    added import habits back

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import HomePage from './HomePage.jsx';
 import Journal from './Journal.jsx';
 import Login from './Login.jsx';
+import Habits from './Habits.jsx';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
