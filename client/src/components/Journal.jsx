dbb53b062e70ecca4a3b6bff71dfb5aeef9e8e49
AlexPHebert2000 <aphebert9994@gmail.com>
2024-05-15 14:09:39 -0500

    ADD Material UI to Journal

--- client/src/components/Journal.jsx
+++ client/src/components/Journal.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import { Typography, Button, TextField } from "@mui/material";
 import JournalBar from "./JournalBar.jsx";
 
 const userId = 2
