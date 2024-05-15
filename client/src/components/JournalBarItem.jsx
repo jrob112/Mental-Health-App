031dae3797275c01ccb1dd7d8a71a711b50360d9
AlexPHebert2000 <aphebert9994@gmail.com>
2024-05-15 11:52:46 -0500

    Merge

--- client/src/components/JournalBarItem.jsx
+++ client/src/components/JournalBarItem.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { Card, CardContent, CardActions, Typography, Button, Box } from "@mui/material";
+import dayjs from "dayjs";
 
 const JournalBarItem = ({ journal }) => {
   return (
-    <>
-      <h1>{journal.title}</h1>
-      <h2>{journal.createdAt}</h2>
-      <p>{journal.body.split('\n')[0]}</p>
-    </>
+    <Card
+    variant="outlined"
+    sx={{width: 1/4, maxHeight: 250, minWidth: 550}}
+    >
+      <CardContent>
+        <Typography variant="h4" gutterBottom>{journal.title}</Typography>
+        <Typography variant="h6" gutterBottom>{dayjs(journal.createdAt).format('MMM-D-YYYY')}</Typography>
+        <Box
+        sx={{maxHeight: 75, overflow: 'hidden'}}
+        >
+          <Typography variant="p" gutterBottom>{journal.body.split('\n')[0]}</Typography>
+        </Box>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Read Entry</Button>
+      </CardActions>
+    </Card>
   )
 }
 
