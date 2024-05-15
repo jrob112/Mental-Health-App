b7a9e470383511031fd7af33f22723214ab27900
Hackman78 <pathenry231@gmail.com>
2024-05-15 16:01:40 -0500

    modified post Habits request handler and added comments

--- server/routers/habitsController.js
+++ server/routers/habitsController.js
@@ -14,9 +14,18 @@ module.exports = {
       });
   },
 
+  /**
+   *  This function post a new Habit to the Database
+   * in the User table and associates a user with their habits
+   *
+   * @param {Request} req
+   * @param {Response} res
+   * @returns {status} 201 || err
+   */
   postHabit: (req, res) => {
     const { UserId } = req.params;
     const { goal, description} = req.body;
+    const { goal, description } = req.body;
 
     const numGoal = Number(goal);
 
@@ -34,6 +43,15 @@ module.exports = {
     .catch((err) => {
       console.error(err);
       res.sendStatus(500);
+      streak: 0,
+      UserId,
     })
+      .then(() => {
+        res.sendStatus(201);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
   },
 };
