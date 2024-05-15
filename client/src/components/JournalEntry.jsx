dae52ee67d417332ee0d4f228b0d93edf2d83772
AlexPHebert2000 <aphebert9994@gmail.com>
2024-05-15 17:59:19 -0500

    ADD edit mode for journal title

--- client/src/components/JournalEntry.jsx
+++ client/src/components/JournalEntry.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useParams } from 'react-router-dom';
-import { Typography } from "@mui/material";
+import { Typography, TextField, Button, ButtonGroup } from "@mui/material";
 import axios from "axios";
 import dayjs from "dayjs";
 
@@ -9,6 +9,8 @@ const userId = 2
 
 const JournalEntry = () => {
   const [journal, setJournal] = useState({})
+  const [title, setTitle] = useState(journal.title)
+  const [editMode, toggleEditMode] = useState(false)
   const journalRef = useRef(journal);
   const { id } = useParams();
 
@@ -20,9 +22,31 @@ const JournalEntry = () => {
       })
   }, [journalRef])
 
+  const cancelEdit = () => {
+    setTitle(journal.title)
+    toggleEditMode(!editMode);
+  }
+
   return (
     <>
-      <Typography variant="h1">{journal.title}</Typography>
+      {
+        editMode ?
+        <TextField
+          value={title}
+          onChange={(e) => {setTitle(e.target.value)}}
+          id="filled-multiline-flexible"
+          label="Title"
+          variant="filled"
+        /> :
+        <Typography variant="h1">{journal.title}</Typography>
+      }
+      {
+        editMode ?
+        <ButtonGroup>
+          <Button variant="contained" color="success" >Submit</Button>
+          <Button variant="contained" color="error" onClick={cancelEdit}>Cancel</Button>
+        </ButtonGroup> :
+        <Button variant="contained" onClick={cancelEdit}>Edit</Button>}
       <Typography variant="h3">{dayjs(journal.createdAt).format('MMM-D-YYYY')}</Typography>
       <Typography variant="p">{journal.body}</Typography>
     </>
