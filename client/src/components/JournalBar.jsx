295bfeeb798f4f12f542425d66cdc1d8b87c1d62
AlexPHebert2000 <aphebert9994@gmail.com>
2024-05-16 09:54:33 -0500

    ADD journal entries to carousel

--- client/src/components/JournalBar.jsx
+++ client/src/components/JournalBar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import JournalBarItem from "./JournalBarItem.jsx";
+import Carousel from 'react-material-ui-carousel'
 
 const JournalBar = ({ journals, deleteJournal }) => {
   return (
-    <>
+    <Carousel
+    >
       {journals.map(journal => <JournalBarItem journal={journal} deleteJournal={deleteJournal} key={journal.id} />)}
-    </>
+    </Carousel>
   )
 }
 
