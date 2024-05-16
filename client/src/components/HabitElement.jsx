e7cc3b7140628c6d54dc2337f4062455943186bc
Hackman78 <pathenry231@gmail.com>
2024-05-15 21:34:56 -0500

    added update and delete function need to add .then block to get habits again

--- client/src/components/HabitElement.jsx
+++ client/src/components/HabitElement.jsx
@@ -1,12 +1,36 @@
+import axios from 'axios';
 import React from 'react';
 
 export default function ({ habit }) {
-  console.log(habit.goal, habit.description, habit.streak);
+  const updateHabit = () => {
+    axios.patch(`/api/${habit.id}/habits`, {
+      goal: habit.goal,
+      timesCompleted: habit.timesCompleted + 1,
+    });
+  };
+
+  const deleteHabit = () => {
+    axios.delete(`/api/${habit.id}/habits`)
+  };
   return (
     <>
       <h3> {habit.description} </h3>
-      <p> Your Daily Goal is { habit.goal }</p>
-      <p> You have a Streak of {habit.streak}! </p>
+      <p> Your Daily Goal is {habit.goal}</p>
+      {(() => {
+        if (habit.timesCompleted >= habit.goal) {
+          return <p> Your Done for the day! Good Job! 🥳</p>;
+        }
+        return (
+          <p> You have completed this habit {habit.timesCompleted} times!!</p>
+        );
+      })()}
+      {() => {
+        if (habit.streak) {
+          return <p> You have a Streak of {habit.streak}! </p>;
+        }
+      }}
+      <button onClick={updateHabit}> Task Completed again</button>
+      <button onClick={deleteHabit}> I'm Tired of this Habit! </button>
     </>
   );
 }
