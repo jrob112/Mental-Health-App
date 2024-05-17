9961793e6ef2538eecc0e71e8c7f5ae1213497cd
Hackman78 <pathenry231@gmail.com>
2024-05-17 11:57:23 -0500

    adding styling

--- client/src/components/HabitElement.jsx
+++ client/src/components/HabitElement.jsx
@@ -1,18 +1,21 @@
 import axios from 'axios';
 import React from 'react';
+import { Fab } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function ({ habit, getAllHabits }) {
   const updateHabit = () => {
-    axios.patch(`/api/${habit.id}/habits`, {
-      goal: habit.goal,
-      timesCompleted: habit.timesCompleted + 1,
-    })
-    .then(getAllHabits);
+    axios
+      .patch(`/api/${habit.id}/habits`, {
+        goal: habit.goal,
+        timesCompleted: habit.timesCompleted + 1,
+      })
+      .then(getAllHabits);
   };
 
   const deleteHabit = () => {
-    axios.delete(`/api/${habit.id}/habits`)
-    .then(getAllHabits)
+    axios.delete(`/api/${habit.id}/habits`).then(getAllHabits);
   };
   return (
     <>
@@ -31,8 +34,24 @@ export default function ({ habit, getAllHabits }) {
           return <p> You have a Streak of {habit.streak}! </p>;
         }
       }}
-      <button onClick={updateHabit}> Task Completed again</button>
-      <button onClick={deleteHabit}> I'm Tired of this Habit! </button>
+      <Fab
+        size='small'
+        color='secondary'
+        aria-label='add'
+        onClick={updateHabit}
+      >
+        <AddIcon sx={{ mr: 1 }} />
+      </Fab>
+      {/* <button onClick={updateHabit}> Task Completed again</button> */}
+      <Fab
+        size='small'
+        color='secondary'
+        aria-label='add'
+        onClick={updateHabit}
+      >
+        <DeleteIcon />
+      </Fab>
+      {/* <button onClick={deleteHabit}> I'm Tired of this Habit! </button> */}
     </>
   );
 }
