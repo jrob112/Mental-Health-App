a44e2e1340f1e009c3579f20bad3bda030e7f36d
Hackman78 <pathenry231@gmail.com>
2024-05-17 19:02:33 -0500

    habit form working with material ui components

--- client/src/components/HabitForm.jsx
+++ client/src/components/HabitForm.jsx
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import React, { useState } from 'react';
+import { TextField, Button, Box, Container, Typography } from '@mui/material';
+import { styleRedButton } from './styles';
+
 
 const userId = 5;
 
@@ -18,23 +21,46 @@ export default function HabitForm({ getAllHabits }) {
   };
 
   return (
-    <form>
-      <input
-        type='text'
-        name='description'
-        value={description}
-        placeholder='description'
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <input
-        type='number'
-        name='goal'
-        placeholder='goal'
-        value={goal}
-        min='1'
-        onChange={(e) => setGoal(Number(e.target.value))}
-      />
-      <button onClick={submitNewHabit}> Submit New Habit </button>
-    </form>
+    <Container>
+      <Typography variant='h4' component='h1' gutterBottom>
+        Add New Habit
+      </Typography>
+      <Box
+        component='form'
+        onSubmit={submitNewHabit}
+        sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
+      >
+        <Box
+          sx={{ display: 'flex', flexDirection: 'row', gap: 2, alignItems: 'flex-end' }}
+        >
+          <TextField
+            label='Habit Description'
+            value={description}
+            id="filled-multiline-flexible"
+            variant="filled"
+            placeholder='Description'
+            onChange={(e) => setDescription(e.target.value)}
+            fullWidth
+            required
+            sx={{ flex: 2 }} // Adjust the flex ratio to give more space to description
+          />
+          <TextField
+            label='Daily Goal'
+            type='number'
+            id="filled-multiline-flexible"
+            variant="filled"
+            value={goal}
+            placeholder='Goal'
+            inputProps={{ min: '1' }}
+            onChange={(e) => setGoal(Number(e.target.value))}
+            required
+            sx={{ flex: 1 }} // Smaller flex ratio as less space is needed for goal
+          />
+        </Box>
+        <Button type='submit' variant='contained' sx={styleRedButton} fullWidth>
+          Submit New Habit
+        </Button>
+      </Box>
+    </Container>
   );
 }
